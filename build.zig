abcfc2f7c5701939bab0e896b30f5a0b08f3c149
Eric Freed <efreed09@gmail.com>
2024-12-24 22:27:59 -0500

    added version to build options

--- build.zig
+++ build.zig
@@ -10,6 +10,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/sodium.zig"),
         .target = target,
         .optimize = optimize,
+        .version = .{ .major = 0, .minor = 1, .patch = 0 },
     });
     b.installArtifact(lib);
 
