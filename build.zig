ec0977d437b1d5e19dec9304d661d25922fd5d37
Eric Freed <efreed09@gmail.com>
2024-12-25 21:06:55 -0500

    added artifact to build script

--- build.zig
+++ build.zig
@@ -4,12 +4,19 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Export as a module
     _ = b.addModule("sodium", .{
         .root_source_file = b.path("src/sodium.zig"),
-        .optimize = optimize,
-        .target = target,
     });
+
+    // Library
+    const lib = b.addStaticLibrary(.{
+        .name = "sodium",
+        .root_source_file = b.path("src/sodium.zig"),
+        .target = target,
+        .optimize = optimize,
+        .version = .{ .major = 0, .minor = 1, .patch = 0 },
+    });
+    b.installArtifact(lib);
 
     // Unit tests
     const unit_tests = b.addTest(.{
@@ -17,7 +24,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
+    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&unit_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
