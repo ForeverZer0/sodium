99fff9a7d4704e2220bf1dd4d9cf5b58d1ec7a29
Eric Freed <efreed09@gmail.com>
2024-12-25 19:28:43 -0500

    updated tests and README

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# Sodium - Command-Line Flag Parser for Zig
+# Sodium
 
-Sodium is a feature-rich command-line flag/option parser for Zig applications, modeled after the top-tier [pflags](https://github.com/spf13/pflag) library for Go. The design goal was to create a complete solution for flag parsing, including auto-generated usage, support for common conventions, while maintaining a stream-lined and intuitive configuration for its users, leveraging the power of Zig's type system.
+Sodium is a feature-rich POSIX/GNU-style flag/option parser for Zig command-line applications. The design goal was to create a complete solution for flag parsing, including auto-generated usage, support for common conventions, while maintaining a stream-lined and intuitive configuration for its users, leveraging the power of Zig's type system.
 
 ## Features
 
-* Handles most common conventions for flag variants:
-  * Long-form: `--flag`, `--flag argument`, `--flag=argument`
-  * Short-form: `-f`, `-f argument`, `-f=argument`, `-fargument`
-  * Short-form (joined): `pacman -Syu`
+* Handles all common style variants:
+  * Long-form: `--flag`, `--flag value`, `--flag=value`
+  * Short-form: `-f`, `-f value`, `-f=value`, `-fvalue`
+  * Short-form (joined): `pacman -Syu` or `rsync -zvhP`
 * Automatic formatted "usage" generation can be displayed with `--help`
-* Uses Zig's type system and comptime features to map arguments for the following types automatically:
+* Uses Zig's strong type system and comptime features to map arguments for the following types automatically:
   * Strings (i.e. `[]const u8`)
   * Integers
   * Floats
@@ -17,8 +17,46 @@ Sodium is a feature-rich command-line flag/option parser for Zig applications, m
   * Enums (by field name and/or numeric value)
   * UTF-8 codepoints (`u21`)
   * Arrays/Vectors
-* Full extensible: implement your own custom types via an interface that requires only a single string parsing function to "just work"
-* Handles interspersed arguments and flags, or choose to disable
-* Supports flags with optional/default arguments
-* By default, displays helpful error messages to users for incorrect/invalid input
+* Fully extensible: implement your own custom types via an interface that requires only a basic parsing function to "just work"
+* Handles interspersed arguments and flags by default, or choose to disable for stricter ordering
+* Supports flags with an optional argument
+* Default values for flags when not specified
+* Displays descriptive error messages to users for incorrect/invalid input
 * Extracts back-quoted variable names to display from arguments in the usage text
+* Respects a naked `--` as a terminator for optional arguments
+
+## Installation
+
+Add an entry to your project's `build.zig.zon`...
+
+```zig
+.dependencies = .{
+    .sodium = .{
+        // Recommended to always use tags
+        .url = "https://github.com/ForeverZer0/sodium/archive/refs/heads/master.tar.gz",
+        // Zig will suggest the correct hash after a (failed) first run by leaving this initially blank
+        .hash = "", 
+    }
+}
+```
+
+...define the module as a dependency in your project's `build.zig`...
+
+```zig
+const sodium = b.dependency("sodium", .{
+    .target = target,
+    .optimize = optimize,
+});
+exe.root_module.addImport("sodium", sodium.module("sodium"));
+```
+
+...and then `@import` and use normally throughout your project:
+
+```zig
+const sodium = @import("sodium");
+const FlagSet = sodium.FlagSet;
+
+const fn main() !void {
+    ...
+}
+```
--- src/flagset_test.zig
+++ src/flagset_test.zig
@@ -9,27 +9,8 @@ test "FlagSet" {
     var flags = try FlagSet.init(allocator, "sodium");
     defer flags.deinit();
 
-    const LogLevel = enum { trace, errors, warnings, info, none };
-
-    var output: []const u8 = "";
-    var verbose: bool = false;
-    var log_level: LogLevel = .none;
-
-    try flags.addFlag([]const u8, "output", 'o', "selects the output `filename`", &output);
-    try flags.setDefault("output", "~/filename.ext");
-    try expectEqualStrings(output, "~/filename.ext");
-
-    try flags.addFlag(bool, "verbose", 'v', "print more information", &verbose);
-
-    try flags.addFlag(LogLevel, "log-level", null, "sets the amount of detail in the log", &log_level);
-    try expectEqual(LogLevel.none, log_level);
-    try flags.setValue("log-level", "warnings");
-    try expectEqual(LogLevel.warnings, log_level);
-}
-
-test "shorthand joined" {
-    var flags = try FlagSet.init(allocator, "sodium");
-    defer flags.deinit();
+    const LogLevel = enum { none, info, warning, errors, trace };
+    const Vec3 = @Vector(3, f32);
 
     var verbose: bool = false;
     var archive: bool = false;
@@ -37,6 +18,8 @@ test "shorthand joined" {
     var sync: bool = false;
     var follow: bool = false;
     var path: []const u8 = &.{};
+    var loglevel: LogLevel = .warning;
+    var direction: Vec3 = undefined;
 
     try flags.addFlag(bool, "verbose", 'v', "verbose output", &verbose);
     try flags.addFlag(bool, "archive", 'a', "preserve file permissions", &archive);
@@ -44,26 +27,42 @@ test "shorthand joined" {
     try flags.addFlag(bool, "sync", 's', "sync database before executing", &sync);
     try flags.addFlag(bool, "follow-links", 'f', "follow symbolic links", &follow);
     try flags.addFlag([]const u8, "output", 'o', "selects the `filename` of the output", &path);
-    try flags.parseArgs(&[_][]const u8{ "-vrs", "--output", "~/filename.ext", "--", "|", "echo", "'hello world'" });
+    try flags.addFlag(LogLevel, "log-level", null, "enable logging with optional `level`", &loglevel);
+    try flags.setDefaultNoOpt("log-level", "warning");
+    try flags.addFlagDefault(Vec3, "direction", 'd', "the world up direction vector", &direction, "0,1,0");
 
+    // Parse arguments
+    try flags.parseArgs(&[_][]const u8{ "-vrrs", "argument", "--output", "~/filename.ext", "--", "|", "echo", "'hello world'" });
+
+    // joined shorthand arguments
     try expectEqual(true, verbose);
     try expectEqual(false, archive);
     try expectEqual(true, recursive);
     try expectEqual(true, sync);
     try expectEqual(false, follow);
-    // try expectEqual(1, flags.len_at_terminator.?);
 
-    const usage_text = try flags.flagUsages(allocator, 0);
+    // Default value being set without parsed from arguments
+    try expectEqual(Vec3{ 0, 1, 0 }, direction);
+    try expectEqual(false, flags.isChanged("direction"));
+
+    // occurrence count
+    try expectEqual(2, flags.getOccurrenceCount("recursive"));
+    try expectEqual(true, flags.isChanged("recursive"));
+    // length at terminator
+    try expectEqual(1, flags.args_before_terminator.?);
+
+    // non-option arguments (interspersed)
+    try expectEqual(4, flags.args.len);
+    const args = [_][]const u8{ "argument", "|", "echo", "hello world" };
+    for (0.., flags.args) |i, arg| try expectEqualStrings(args[i], arg);
+
+    const usage_text = try flags.usageText(allocator, 0);
     defer allocator.free(usage_text);
 
-    // Argument name extraction
+    // argument name extraction
     try expectEqual(true, std.mem.indexOf(u8, usage_text, "output <filename>") != null);
-
-    try expectEqual(3, flags.args.len);
-    try expectEqualStrings("|", flags.args[0]);
-    try expectEqualStrings("echo", flags.args[1]);
-    try expectEqualStrings("hello world", flags.args[2]);
-
-    std.debug.print("{s}", .{usage_text});
-    for (flags.args) |arg| std.debug.print("{s}\n", .{arg});
+    // argument name extraction with optional flag value
+    try expectEqual(true, std.mem.indexOf(u8, usage_text, "log-level [level=\"warning\"]") != null);
+    // visual test, just ensure text alignment looks correct
+    std.debug.print("\n########## VISUAL ALIGNMENT ###########\n\n{s}\n#######################################\n", .{usage_text});
 }
